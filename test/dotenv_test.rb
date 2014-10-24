00b41da644888917bf03173ee6d329979194c501
csquared <christopher.continanza@gmail.com>
2014-10-23 20:26:48 -0700

    .env loading

---
+++ test/dotenv_test.rb
@@ -0,0 +1,51 @@
+require 'helper'
+
+class DotenvTest < Test 
+  def setup
+    super
+    FakeFS.activate!
+  end
+
+  def teardown
+    super
+    %w{.env .env.local .env.test}.each do |f|
+      File.unlink(f) if File.exists?(f)
+    end
+    FakeFS.deactivate!
+  end
+
+  def test_one_var
+    File.open(".env",'w') { |f| f << "FOO=bar" }
+    assert_equal(nil, Config[:foo])
+    Config.dotenv!
+    assert_equal('bar', Config[:foo])
+  end
+  
+  def test_two_files
+    File.open(".env",'w') { |f| f << "FOO=bar" }
+    File.open(".env.local",'w') { |f| f << "FOO=zzz" }
+    assert_equal(nil, Config[:foo])
+    Config.dotenv!
+    assert_equal('zzz', Config[:foo])
+  end
+
+  def test_env_specific
+    File.open(".env",'w') { |f| f << "FOO=bar" }
+    File.open(".env.local",'w') { |f| f << "FOO=zzz" }
+    File.open(".env.test",'w') { |f| f << "FOO=test" }
+    assert_equal(nil, Config[:foo])
+    set_env('RACK_ENV', 'test')
+    Config.dotenv!
+    assert_equal('test', Config[:foo])
+  end
+
+  def test_noop_in_prod
+    File.open(".env",'w') { |f| f << "FOO=bar" }
+    File.open(".env.local",'w') { |f| f << "FOO=zzz" }
+    File.open(".env.test",'w') { |f| f << "FOO=test" }
+    assert_equal(nil, Config[:foo])
+    set_env('RACK_ENV', 'production')
+    Config.dotenv!
+    assert_equal(nil, Config[:foo])
+  end
+end
